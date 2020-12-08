390cc9b2f7b91254818537f52f906d4b848cc13b
Aleksey Serezhin <nagus128@gmail.com>
2020-12-09 02:19:03 +0300

    Fixed package config

--- Package.swift
+++ Package.swift
@@ -23,10 +23,6 @@ let package = Package(
         .target(
             name: "SmartSegue",
             dependencies: []
-        ),
-        .testTarget(
-            name: "SmartSegueTests",
-            dependencies: ["SmartSegue"]
         )
     ]
 )
